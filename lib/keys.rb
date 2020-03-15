23eac515a06a136a4fcd74e364d2ed0d09352a65
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 10:48:01 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,14 +1,1 @@
 require 'pry'
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
-  meat: ["chicken", "steak", "salmon"],
-  grains: ["rice", "pasta"]
-}
-def get_the_keys(groceries)
- groceries.keys
-end
-
-def get_the_values(groceries)
-  groceries.values 
-end
